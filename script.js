cc963f62e46e12d4f2bc6fee0f119bf89a3803d5
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-15 22:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let age=document.getElementById('age')
+let name=document.getElementById('name')
+function promiseChaining(event){
+	event.preventDefault()
+	console.log('check')
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			if(age.value>=18){
+				reject("checking")
+			}
+		},4000)
+	})
+}
+
+promiseChaining().then((res) => {
+	console.log('bhbjk')
+	return res
+}).catch( (e) => console.log('error',e))
