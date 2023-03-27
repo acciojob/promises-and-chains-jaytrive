d7b70870a1b1115b15425a2f728b631a7782b87e
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-27 16:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,23 @@
 let age=document.getElementById('age')
 let name=document.getElementById('name')
 function promiseChaining(event){
-	event.preventDefault()
-	console.log('check')
-	return new Promise((resolve,reject) => {
+	event.preventDefault();
+	if(!age.value || !name.value){
+		alert("Please enter valid details");
+		return;
+	}
+	
+	const promise= new Promise((resolve,reject) => {
 		setTimeout(() => {
-			if(age.value>=18){
-				reject("checking")
+			if(Number(age.value)>=18){
+				resolve(`Welcome, ${name.value}. You can vote.`)
+			}else{
+				reject(`sorry ${name.value}. You aren't old enough.`);
 			}
 		},4000)
 	})
+	promise.then((res) => {
+		alert(res);
+	}).catch((e) => alert(e))
 }
 
-promiseChaining().then((res) => {
-	console.log('bhbjk')
-	return res
-}).catch( (e) => console.log('error',e))
