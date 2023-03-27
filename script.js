49216a23788106708a41c5799754dfa630e95e81
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-27 17:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 //your JS code here. If required.
-let age=document.getElementById('age')
-let name=document.getElementById('name')
-function promiseChaining(event){
-	event.preventDefault();
-	if(!age.value || !name.value){
-		alert("Please enter valid details");
-		return;
-	}
-	
-	const promise= new Promise((resolve,reject) => {
-		setTimeout(() => {
-			if(Number(age.value)>=18){
-				resolve(`Welcome, ${name.value}. You can vote.`)
-			}else{
-				reject(`sorry ${name.value}. You aren't old enough.`);
-			}
-		},4000)
-	})
-	promise.then((res) => {
-		alert(res);
-	}).catch((e) => alert(e))
-}
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const submitBtn = document.getElementById('btn');
+
+// Add a listener to the form submission event
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Check if the inputs are valid
+  if (!nameInput.value || !ageInput.value) {
+    alert('Please enter valid details');
+    return;
+  }
+
+  // Create a promise that resolves after 4 seconds with an object
+  const delayPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Number(ageInput.value) >= 18) {
+        resolve({ name: nameInput.value, age: Number(ageInput.value) });
+      } else {
+        reject({ name: nameInput.value });
+      }
+    }, 4000);
+  });
+
+  // Handle the result of the promise
+  delayPromise
+    .then((data) => {
+      alert(`Welcome, ${data.name}. You can vote.`);
+    })
+    .catch((error) => {
+      alert(`Oh sorry ${error.name}. You aren't old enough.`);
+    });
+});
 
